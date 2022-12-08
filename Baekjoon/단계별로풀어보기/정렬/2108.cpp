ed6d8090afd9cf33babe684a0fbb4aed1fa98568
김동성 <mekind98@gmail.com>
2022-12-08 10:36:50 +0900

    몸풀기

--- Baekjoon/단계별로풀어보기/정렬/2108.cpp
+++ Baekjoon/단계별로풀어보기/정렬/2108.cpp
@@ -5,68 +5,66 @@
 
 using namespace std;
 typedef long long ll;
-ll n, arr[500010];
-ll sum = 0;
+
+ll n;
+ll arr[500010];
+ll avg, mid, many, range;
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    cin >> n;
 
+    cin>>n;
     for (ll i = 0LL; i < n; i++)
     {
-        cin >> arr[i];
-
-        sum += arr[i];
+        cin>>arr[i];
+        avg+=arr[i];
     }
-    sort(arr, arr + n);
 
-    bool p = false;
-    if (sum < 0)
-        p = true;
-    sum = abs(sum);
-    ll avg = sum << 1;
-    avg /= n;
-    ll da = 0LL;
-
-    if (avg & 1)
-        avg = (avg >> 1) + 1LL;
-    else
-        avg = (avg >> 1);
-    if (p)
-        avg = 0 - avg;
-    int cnt = 0;
-    bool two = false;
-
-    for (int i = 0; i < n; ++i)
+    sort(arr, arr+n);
+    bool ch = false;
+    if (avg < 0)
     {
-        ll now = arr[i];
-        int now_cnt = 0;
-        for (ll j = i;; j++)
-        {
-
-            if (now != arr[j])
-                break;
-            now_cnt += 1;
-        }
-        if (now_cnt == now)
-        {
-            if (two)
-            {
-                da = now;
-                two = false;
-            }
-        }
-        if (now_cnt > now)
-        {
-            cnt = now_cnt;
-            da = now;
-            two = true;
-        }
+        ch = true;
+        avg = abs(avg);
     }
+    
+    avg = avg<<1;
+    avg/=n;
+    avg = (avg>>1) + (avg&1LL);
+    if(ch)
+        avg = 0LL- avg;
+    
+    ll mid_idx = n>>1;
+    mid = arr[mid_idx];
 
-    cout << avg << "\n";
-    cout << arr[(int)(n >> 1)] << "\n";
-    cout << da << "\n";
-    cout << arr[n - 1] - arr[0] << "\n";
-}
+    int many_cnt =0;
+    bool two =true;
+    for (ll i = 0; i < n; i++)
+    {
+        int now_cnt = 0;
+        for (ll j = i; arr[j] ==arr[i]; j++)
+        {
+            now_cnt +=1;
+        }
+        i+=now_cnt-1;
+        if (many_cnt == now_cnt && two){
+            many = arr[i];
+            two = false;
+        }
+        if(many_cnt < now_cnt){
+            many = arr[i];
+            many_cnt = now_cnt;
+            two=true;
+        }
+
+    }
+    
+
+    range = arr[n-1] - arr[0];
+    cout<<avg<<"\n";
+    cout<<mid<<"\n";
+    cout<<many<<"\n";
+    cout<<range <<"\n";
+};
